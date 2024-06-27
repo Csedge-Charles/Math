c80fc042257ea191df03e5cbe3483d02b6a3e517
Charles <charles.shao@csedge.org>
2024-06-27 12:50:22 -0700

    radians vs degrees

--- trig.py
+++ trig.py
@@ -4,21 +4,11 @@ class MyMath:
         radians %= 24
         return radians
     
-def sin(x, type):
-    if type == 'degrees' or type == 'd' or type == 'degree' or type == 'Degrees' or type == 'Degree' or type == 'D':
+
+class degrees:
+    def sin(x):
         x %= 360
         degree_list = [0, 30, 45, 60, 90, 120, 135, 150, 180, 210, 225, 240, 270, 300, 315, 330]
-        sin_list = ['0', '1/2', '√2/2', '√3/2', '1', '√3/2', '√2/2', '1/2', '0', '-1/2', '-√2/2', 
-                    '-√3/2', '-1', '-√3/2', '-√2/2', '-1/2']
-        y = 0
-        for i in range(len(degree_list)):
-            if degree_list[y] == x:
-                break
-            y += 1
-        return sin_list[y]
-    else:
-        x = MyMath.convert(x)
-        degree_list = [0, 2, 3, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22]
         sin_list = ['0', '1/2', '√2/2', '√3/2', '1', '√3/2', '√2/2', '1/2', '0', '-1/2', '-√2/2', 
                     '-√3/2', '-1', '-√3/2', '-√2/2', '-1/2']
         y = 0
@@ -28,49 +18,72 @@ def sin(x, type):
             y += 1
         return sin_list[y]
 
-def cos(x, type):
-    if type == 'degrees' or type == 'd' or type == 'degree' or type == 'Degrees' or type == 'Degree' or type == 'D':
+    def cos(x):
         x %= 360
         degree_list = [0, 30, 45, 60, 90, 120, 135, 150, 180, 210, 225, 240, 270, 300, 315, 330]
-        sin_list = ['1', '√3/2', '√2/2', '1/2', '0', '-1/2', '-√2/2', '-√3/2', '-1', '-√3/2', '-√2/2', 
+        cos_list = ['1', '√3/2', '√2/2', '1/2', '0', '-1/2', '-√2/2', '-√3/2', '-1', '-√3/2', '-√2/2', 
                     '-1/2', '0', '1/2', '√2/2', '-3/2']
         y = 0
         for i in range(len(degree_list)):
             if degree_list[y] == x:
                 break
             y += 1
-        return sin_list[y]
-    else:
+        return cos_list[y]
+    def tan(x):
+        x %= 360
+        degree_list = [0, 30, 45, 60, 90, 120, 135, 150, 180, 210, 225, 240, 270, 300, 315, 330]
+        tan_list = ['0', '√3/3', '1', '√3', 'undefined', '-√3', '-1', '-√3/3', '0', '√3/3', '1', 
+                    '√3', 'undefined', '-√3', '-1', '-√3/3']
+        y = 0
+        for i in range(len(degree_list)):
+            if degree_list[y] == x:
+                break
+            y += 1
+        return tan_list[y]
+    
+
+
+
+class radians:
+
+
+    def sin(x):
         x = MyMath.convert(x)
         degree_list = [0, 2, 3, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22]
-        sin_list = ['1', '√3/2', '√2/2', '1/2', '0', '-1/2', '-√2/2', '-√3/2', '-1', '-√3/2', '-√2/2', 
+        sin_list = ['0', '1/2', '√2/2', '√3/2', '1', '√3/2', '√2/2', '1/2', '0', '-1/2', '-√2/2', 
+                    '-√3/2', '-1', '-√3/2', '-√2/2', '-1/2']
+        y = 0
+        for i in range(len(degree_list)):
+            if degree_list[y] == x:
+                break
+            y += 1
+        return sin_list[y]
+    
+
+    def cos(x):
+        x = MyMath.convert(x)
+        degree_list = [0, 2, 3, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22]
+        cos_list = ['1', '√3/2', '√2/2', '1/2', '0', '-1/2', '-√2/2', '-√3/2', '-1', '-√3/2', '-√2/2', 
                     '-1/2', '0', '1/2', '√2/2', '-√3/2']
         y = 0
         for i in range(len(degree_list)):
             if degree_list[y] == x:
                 break
             y += 1
-        return sin_list[y]
-def tan(x, type):
-    if type == 'degrees' or type == 'd' or type == 'degree' or type == 'Degrees' or type == 'Degree' or type == 'D':
-        x %= 360
-        degree_list = [0, 30, 45, 60, 90, 120, 135, 150, 180, 210, 225, 240, 270, 300, 315, 330]
-        sin_list = ['0', '√3/3', '1', '√3', 'undefined', '-√3', '-1', '-√3/3', '0', '√3/3', '1', 
-                    '√3', 'undefined', '-√3', '-1', '-√3/3']
-        y = 0
-        for i in range(len(degree_list)):
-            if degree_list[y] == x:
-                break
-            y += 1
-        return sin_list[y]
-    else:
+        return cos_list[y]
+    
+
+    def tan(x):
         x = MyMath.convert(x)
         degree_list = [0, 2, 3, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22]
-        sin_list = ['0', '√3/3', '1', '√3', 'undefined', '-√3', '-1', '-√3/3', '0', '√3/3', '1', 
+        tan_list = ['0', '√3/3', '1', '√3', 'undefined', '-√3', '-1', '-√3/3', '0', '√3/3', '1', 
                     '√3', 'undefined', '-√3', '-1', '-√3/3']
         y = 0
         for i in range(len(degree_list)):
             if degree_list[y] == x:
                 break
             y += 1
-        return sin_list[y]
+        return tan_list[y]
+
+
+print(degrees.sin(30))
